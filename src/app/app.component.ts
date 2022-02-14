d4bd692bff921301498ba8b8c1333bcee7d362d8
Babak <babak.jahanbani@yahoo.com>
2022-02-14 14:04:48 +0330

    ngClass & ngFor & else &ng-template & ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,13 @@ export class AppComponent {
     // }
     // return '';
   }
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
+  changePage() {
+    this.currentPage -= 1;
+  }
+
   images = [
     {
       title: 'BMW X4 M40i 2022',
@@ -31,5 +38,55 @@ export class AppComponent {
       title: 'Mercedes AMG G63 4MATIC 2022',
       url: 'https://www.ccarprice.com/products/Mercedes_AMG_G63_2022.jpg',
     },
+    {
+      title: 'BMW X4 M40i 2022',
+      url: 'https://www.ccarprice.com/products/BMW_X4_M40i_2022_1.jpg',
+    },
+    {
+      title: 'Volvo XC60 B6 R-Design 2022',
+      url: 'https://www.ccarprice.com/products/Volvo_XC60_B6_R-Design_2022.jpg',
+    },
+    {
+      title: 'Jeep Wrangler Unlimited Sahara Altitude 2022',
+      url: 'https://www.ccarprice.com/products/Jeep_Wrangler_Unlimited_Sahara_Altitude_2022.jpg',
+    },
+    {
+      title: 'Mercedes AMG G63 4MATIC 2022',
+      url: 'https://www.ccarprice.com/products/Mercedes_AMG_G63_2022.jpg',
+    },
+    {
+      title: 'BMW X4 M40i 2022',
+      url: 'https://www.ccarprice.com/products/BMW_X4_M40i_2022_1.jpg',
+    },
+    {
+      title: 'Volvo XC60 B6 R-Design 2022',
+      url: 'https://www.ccarprice.com/products/Volvo_XC60_B6_R-Design_2022.jpg',
+    },
+    {
+      title: 'Jeep Wrangler Unlimited Sahara Altitude 2022',
+      url: 'https://www.ccarprice.com/products/Jeep_Wrangler_Unlimited_Sahara_Altitude_2022.jpg',
+    },
+    {
+      title: 'Mercedes AMG G63 4MATIC 2022',
+      url: 'https://www.ccarprice.com/products/Mercedes_AMG_G63_2022.jpg',
+    },
+    {
+      title: 'BMW X4 M40i 2022',
+      url: 'https://www.ccarprice.com/products/BMW_X4_M40i_2022_1.jpg',
+    },
+    {
+      title: 'Volvo XC60 B6 R-Design 2022',
+      url: 'https://www.ccarprice.com/products/Volvo_XC60_B6_R-Design_2022.jpg',
+    },
+    {
+      title: 'Jeep Wrangler Unlimited Sahara Altitude 2022',
+      url: 'https://www.ccarprice.com/products/Jeep_Wrangler_Unlimited_Sahara_Altitude_2022.jpg',
+    },
+    {
+      title: 'Mercedes AMG G63 4MATIC 2022',
+      url: 'https://www.ccarprice.com/products/Mercedes_AMG_G63_2022.jpg',
+    },
   ];
+
+  isExist: boolean = false;
 }
