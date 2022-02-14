239c60bb172475fa6057d02c702dc5bd561f7312
Babak <babak.jahanbani@yahoo.com>
2022-02-14 19:40:51 +0330

    HostListener, HostBinding,

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,15 +1,35 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
 @Directive({
   selector: '[appClass]',
 })
 export class ClassDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
     // this.elementRef.nativeElement.style.backgroundColor = '#B5BCBF';
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      '#B5BCBF'
-    );
+    // this.renderer.setStyle(this.elementRef.nativeElement,'background-color','#B5BCBF');
+  }
+  @HostListener('mouseenter') onMouseOver() {
+    this.backgroundColor = '#B5BCBF';
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   '#B5BCBF'
+    // );
+  }
+  @HostListener('mouseleave') onMouseOut() {
+    this.backgroundColor = 'transparent';
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    // );
   }
 }
