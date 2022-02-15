14fff2f57beb633c724452de0e22baeed209b073
Babak <babak.jahanbani@yahoo.com>
2022-02-15 18:04:43 +0330

    Create Attribute Directive instead of ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ClassDirective } from './class.directive';
 import { Class2Directive } from './class2.directive';
+import { SetClassDirective } from './set-class.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClassDirective,
-    Class2Directive
+    Class2Directive,
+    SetClassDirective
   ],
   imports: [
     BrowserModule
